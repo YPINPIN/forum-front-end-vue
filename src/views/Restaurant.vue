57f59d85105653d4851d8f758a13483e26cf0c8d
YPINPIN <yping611@gmail.com>
2021-08-29 00:45:32 +0800

    [ADD] Add RestaurantComments & mixins

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -5,12 +5,14 @@
     <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
+    <RestaurantComments :restaurant-comments="restaurantComments" />
     <!-- 新增評論 CreateComment -->
   </div>
 </template>
 
 <script>
 import RestaurantDetail from './../components/RestaurantDetail'
+import RestaurantComments from './../components/RestaurantComments'
 
 const dummyData = {
   restaurant: {
@@ -194,6 +196,7 @@ const dummyData = {
 export default {
   components: {
     RestaurantDetail,
+    RestaurantComments,
   },
   data() {
     return {
