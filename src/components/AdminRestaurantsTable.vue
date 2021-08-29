4d54bd18b214e9e459eda7a2cab89d790ce4f005
YPINPIN <yping611@gmail.com>
2021-08-29 18:24:58 +0800

    [ADD] Add delete admin restaurant function

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -33,7 +33,11 @@
 
           <a href="#" class="btn btn-link">Edit</a>
 
-          <button type="button" class="btn btn-link">
+          <button
+            type="button"
+            class="btn btn-link"
+            @click.stop.prevent="deleteRestaurant(restaurant.id)"
+          >
             Delete
           </button>
         </td>
@@ -1024,6 +1028,11 @@ export default {
   methods: {
     fetchRestaurants() {
       this.restaurants = dummyData.restaurants
+    },
+    deleteRestaurant(restaurantId) {
+      this.restaurants = this.restaurants.filter(
+        (restaurant) => restaurant.id !== restaurantId
+      )
     },
   },
 }
