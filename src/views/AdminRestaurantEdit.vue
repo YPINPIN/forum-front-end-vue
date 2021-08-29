ec88a9f394a69c1c798748c00d5f156e9d62b9a1
YPINPIN <yping611@gmail.com>
2021-08-29 20:08:42 +0800

    [ADD] Add AdminRestaurantEdit

---
+++ src/views/AdminRestaurantEdit.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container py-5">
+    <AdminRestaurantForm
+      :initial-restaurant="restaurant"
+      @after-submit="handleAfterSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import AdminRestaurantForm from './../components/AdminRestaurantForm.vue'
+
+const dummyData = {
+  restaurant: {
+    id: 4,
+    name: 'Mckayla Hintz',
+    tel: '1-240-532-2429',
+    address: '167 Rogahn Mission',
+    opening_hours: '08:00',
+    description: 'Impedit nesciunt ad id delectus magnam aspernatur fugit.',
+    image:
+      'https://loremflickr.com/320/240/restaurant,food/?random=42.58020446504014',
+    viewCounts: 41,
+    createdAt: '2021-07-05T09:58:39.000Z',
+    updatedAt: '2021-08-29T08:30:03.000Z',
+    CategoryId: 4,
+    Category: {
+      id: 4,
+      name: '墨西哥料理',
+      createdAt: '2021-07-05T09:58:39.000Z',
+      updatedAt: '2021-07-05T09:58:39.000Z',
+    },
+  },
+}
+
+export default {
+  components: {
+    AdminRestaurantForm,
+  },
+  data() {
+    return {
+      restaurant: {
+        id: -1,
+        name: '',
+        categoryId: '',
+        tel: '',
+        address: '',
+        description: '',
+        image: '',
+        openingHours: '',
+      },
+    }
+  },
+  created() {
+    const { id } = this.$route.params
+    this.fetchRestaurant(id)
+  },
+  methods: {
+    handleAfterSubmit(formData) {
+      // 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ': ' + value)
+      }
+    },
+    fetchRestaurant(restaurantId) {
+      console.log('fetchRestaurant id:', restaurantId)
+      const { restaurant } = dummyData
+      this.restaurant = {
+        ...this.restaurant,
+        id: restaurant.id,
+        name: restaurant.name,
+        categoryId: restaurant.CategoryId,
+        tel: restaurant.tel,
+        address: restaurant.address,
+        description: restaurant.description,
+        image: restaurant.image,
+        openingHours: restaurant.opening_hours,
+      }
+    },
+  },
+}
+</script>
